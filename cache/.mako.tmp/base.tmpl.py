80fc47a58e986c3e975c9b1f7ec27cb92382e6d8
gramanas <anastasis.gramm2@gmail.com>
2018-05-30 15:05:03 +0300

    Nikola auto commit.
    Nikola version: 7.8.13

--- cache/.mako.tmp/base.tmpl.py
+++ cache/.mako.tmp/base.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1527598439.2340062
+_modified_time = 1527681903.2874393
 _enable_loop = True
 _template_filename = 'themes/lotabout/templates/base.tmpl'
 _template_uri = 'base.tmpl'
@@ -41,17 +41,17 @@ def render_body(context,**pageargs):
         _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'annotations')._populate(_import_ns, ['*'])
-        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
-        header = _mako_get_namespace(context, 'header')
-        zzz = _mako_get_namespace(context, 'zzz')
-        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
-        def content():
-            return render_content(context._locals(__M_locals))
-        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
-        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
-        footer = _mako_get_namespace(context, 'footer')
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
+        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
+        footer = _mako_get_namespace(context, 'footer')
+        header = _mako_get_namespace(context, 'header')
+        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
+        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        zzz = _mako_get_namespace(context, 'zzz')
+        def content():
+            return render_content(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
