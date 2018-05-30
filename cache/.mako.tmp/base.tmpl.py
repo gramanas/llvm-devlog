97a145da3d552f2cdf1df6c6f05e790f18139277
gramanas <anastasis.gramm2@gmail.com>
2018-05-30 18:18:06 +0300

    Nikola auto commit.
    Nikola version: 7.8.13

--- cache/.mako.tmp/base.tmpl.py
+++ cache/.mako.tmp/base.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1527682408.1322353
+_modified_time = 1527693484.7795095
 _enable_loop = True
 _template_filename = 'themes/lotabout/templates/base.tmpl'
 _template_uri = 'base.tmpl'
@@ -41,17 +41,17 @@ def render_body(context,**pageargs):
         _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'annotations')._populate(_import_ns, ['*'])
-        zzz = _mako_get_namespace(context, 'zzz')
-        def content():
-            return render_content(context._locals(__M_locals))
-        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
         lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
         header = _mako_get_namespace(context, 'header')
-        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
-        footer = _mako_get_namespace(context, 'footer')
         body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
+        def content():
+            return render_content(context._locals(__M_locals))
+        zzz = _mako_get_namespace(context, 'zzz')
+        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        footer = _mako_get_namespace(context, 'footer')
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
